f8e55e7899a4d5eac59f550a88b9bce018a25550
Michael Burke <michael_burke4@student.uml.edu>
2023-10-01 15:12:15 -0400

    Adjust text placement + bg box

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from PIL import Image, ImageFont, ImageDraw
 import textwrap
 
-IMG_DIMENSIONS = (1280, 720)
+FRAME_DIMENSIONS = (1280, 720)
 
 FONT_SIZE = 100
 TEXT_SPACING = 66
 LINE_MAX_CHARS = 14
+LOGO_LOC = (50, 50)
 
 KDLP_YELLOW = (255, 255, 209)
 KDLP_RED = (214, 76, 55)
@@ -24,17 +25,24 @@ def find_longest_line(wrap):
             longest = len(string)
     return longest
 
-img = Image.new(mode='RGB', size=IMG_DIMENSIONS, color=(255,255,255))
+img = Image.new(mode="RGB", size=FRAME_DIMENSIONS, color=(255,255,255))
 logo = Image.open("kdlp_logo.png")
-logo_width = logo.getbbox()[2]
-img.paste(logo, (50, 50))
+LOGO_WIDTH = logo.getbbox()[2]
+img.paste(logo, LOGO_LOC)
 draw = ImageDraw.Draw(img)
 
-courier = ImageFont.truetype("Courier.ttc",FONT_SIZE)
+courier = None
+try:
+    courier = ImageFont.truetype("Courier.ttc",FONT_SIZE)
+except:
+    try:
+        courier = ImageFont.truetype("courier.ttf", FONT_SIZE)
+    except:
+        print("COULD NOT LOCATE COURIER FONT. EXITING...")
+        exit(1)
+
 
 text = "Bash Tips and Tricks"
-# text = "Linux VM Setup on an Arm64 MacOS Host"
-# text = "Short Title"
 wrap = textwrap.wrap(text, width=LINE_MAX_CHARS)
 print(wrap)
 print(find_longest_line(wrap))
@@ -44,12 +52,13 @@ wraplen = len(wrap)
 TEXTBOX_HEIGHT = (wraplen * (FONT_SIZE * .75)) + ((wraplen - 1) * TEXT_SPACING)
 TEXTBOX_WIDTH = find_longest_line(wrap) * (FONT_SIZE * .6)
 
-TEXT_TOPLEFT = (350, IMG_DIMENSIONS[1]/2 - TEXTBOX_HEIGHT/2 - 50)
+LOGO_OFFSET = LOGO_LOC[0] + LOGO_WIDTH
+TEXT_TOPLEFT = (((FRAME_DIMENSIONS[0] - LOGO_OFFSET)/2) + LOGO_OFFSET - (TEXTBOX_WIDTH/2), FRAME_DIMENSIONS[1]/2 - TEXTBOX_HEIGHT/2 - 50)
 
 bg_x1 = TEXT_TOPLEFT[0] - (FONT_SIZE * .1)
-bg_y1 = TEXT_TOPLEFT[1] #There is already a bit of padding on the top of the text box
+bg_y1 = TEXT_TOPLEFT[1] # There is already a bit of padding on the top of the text box
 bg_x2 = TEXT_TOPLEFT[0] + TEXTBOX_WIDTH + (FONT_SIZE * .1)
-bg_y2 = TEXT_TOPLEFT[1] +  TEXTBOX_HEIGHT + (FONT_SIZE * .25)# 2.5x padding to accomodate default upper padding
+bg_y2 = TEXT_TOPLEFT[1] +  TEXTBOX_HEIGHT + (FONT_SIZE * .25) # 2.5x padding to mirror default upper padding
 
 acc_x1 = bg_x1
 acc_y1 = bg_y2
@@ -58,6 +67,5 @@ acc_y2 = acc_y1 + 5
 
 draw.rectangle((bg_x1, bg_y1, bg_x2, bg_y2), fill=KDLP_YELLOW)
 draw.rectangle((acc_x1, acc_y1, acc_x2, acc_y2), fill=KDLP_RED)
-# draw.rectangle()
-draw.multiline_text(TEXT_TOPLEFT, wrap_join(wrap), font=courier, fill=(0,0,0), spacing=TEXT_SPACING)
+draw.multiline_text(TEXT_TOPLEFT, wrap_join(wrap), font=courier, fill=(0,0,0), spacing=TEXT_SPACING, align='center')
 img.show()
